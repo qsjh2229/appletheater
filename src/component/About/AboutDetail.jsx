b275d531e0b105a3650d9e94777ffa9629388697
cheawook <qsjh2229@naver.com>
2023-09-04 15:08:28 +0900

    탭 수정

--- src/component/About/AboutDetail.jsx
+++ src/component/About/AboutDetail.jsx
@@ -1,20 +1,59 @@
-import React from 'react';
+/* import React, { useState } from 'react';
 import DetailInfo from './DetailInfo';
 import AboutRev from './AboutRev';
 import styles from "./AboutDetail.module.scss"
 const AboutDetail = ({movie}) => {
+   let [tab,setTab]=useState(0)
    return (
       
       <section className={styles.aboutTab}>
          <ul>
-         <li className={`${styles.abTabbtn} ${styles.active}`}>상세보기</li>
-            <li className={styles.abTabbtn}>관람평</li>
+         <li className={`${styles.abTabbtn} ${styles.active}`} onClick={()=>{ setTab(0) }} eventKey="link0">상세보기</li>
+            <li className={styles.abTabbtn} onClick={()=>{setTab(1)}  }eventKey="link1">관람평</li>
         </ul>
         <div>
-        <DetailInfo movie={movie} />
-         <AboutRev></AboutRev>
+        <DetailInfo movie={movie} tab={0} />
+         <AboutRev  tab={1}></AboutRev>
         </div>
          
+      </section>
+   );
+};
+
+export default AboutDetail; */
+import React, { useState } from 'react';
+import DetailInfo from './DetailInfo';
+import AboutRev from './AboutRev';
+import styles from "./AboutDetail.module.scss"
+
+const AboutDetail = ({ movie }) => {
+   const [tab, setTab] = useState(0);
+
+   return (
+      <section className={styles.aboutTab}>
+         <ul>
+            <li
+               className={`${styles.abTabbtn} ${tab === 0 ? styles.active : ''}`}
+               onClick={() => { setTab(0) }}
+               eventKey="link0"
+            >
+               상세보기
+            </li>
+            <li
+               className={`${styles.abTabbtn} ${tab === 1 ? styles.active : ''}`}
+               onClick={() => { setTab(1) }}
+               eventKey="link1"
+            >
+               관람평
+            </li>
+         </ul>
+         <div>
+            {tab === 0 ? (
+               <DetailInfo movie={movie} />
+            ) : (
+               <AboutRev tab={tab} />
+            )}
+         </div>
       </section>
    );
 };
--- src/component/About/AboutMap.jsx
+++ src/component/About/AboutMap.jsx
@@ -1,55 +1,79 @@
-import { AiFillStar } from "react-icons/ai"; 
-import React, { useState } from "react";
+import { AiFillStar } from "react-icons/ai";
+import React, { useState, useEffect } from "react";
 import styles from "./AboutRev.module.scss";
 import { BsFillHandThumbsDownFill } from "react-icons/bs";
 import { BsFillHandThumbsUpFill } from "react-icons/bs";
 
 const AboutMap = (props) => {
-   const [down, setDown] = useState(51);
-   const [up, setUp] = useState([15]); // 배열로 초기화
+  const [commentData, setCommentData] = useState([]);
+  const [voteData, setVoteData] = useState([]);
 
-   const upHandler = (idx) => {
-      // 해당 인덱스에 대한 'up' 상태를 업데이트합니다.
-      let copy = [...up];
-      copy[idx] = (copy[idx] || 0) + 1; // 기본값 0을 사용하고, 해당 인덱스의 값에 1을 더합니다.
-      setUp(copy);
-   };
+  const upHandler = (commentIdx, voteIdx) => {
+    const updatedVoteData = [...voteData];
+    updatedVoteData[commentIdx][voteIdx].upVotes += 1;
+    setVoteData(updatedVoteData);
+  };
 
-   return (
-      <>
-         {props.commenttxt.map((item, idx) => (
-            <div className={styles.tBox} key={idx}>
-               <div className={styles.star}><AiFillStar />
-               <AiFillStar />
-               <AiFillStar />
-               <AiFillStar />
-               <AiFillStar />
-               </div>
-               <div className={styles.desRebox}>
-                  <span>{item} </span>
-               </div>
-               <div className={styles.infRebox}>
-                  <div className={styles.idbox}>
-                     <span className={styles.date}>20230646</span>{" "}
-                     <span className={styles.id}>영화과 ㅇ채욱</span>
-                  </div>
-                  <div className={styles.reBtn}>
-                     <BsFillHandThumbsUpFill
-                        className='rePBtn'
-                        onClick={upHandler}
-                     />
-                    <span> {up}</span>
-                     <BsFillHandThumbsDownFill
-                        className='rePmtn'
-                        onClick={downHandler}
-                     />
-                    <span> {down}</span>
-                  </div>
-               </div>
+  const downHandler = (commentIdx, voteIdx) => {
+    const updatedVoteData = [...voteData];
+    updatedVoteData[commentIdx][voteIdx].downVotes += 1;
+    setVoteData(updatedVoteData);
+  };
+
+  useEffect(() => {
+    // props.commenttxt 배열의 길이가 변경될 때, commentData와 voteData를 업데이트합니다.
+    const newCommentData = props.commenttxt.map((item) => ({ text: item }));
+    const newVoteData = newCommentData.map(() => ([
+      { upVotes: 0, downVotes: 0 }
+    ]));
+
+    setCommentData(newCommentData);
+    setVoteData(newVoteData);
+  }, [props.commenttxt]);
+
+  return (
+    <>
+      {commentData.map((comment, commentIdx) => (
+        <div className={styles.tBox} key={commentIdx}>
+          <div className={styles.star}>
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+          </div>
+          <div className={styles.desRebox}>
+            <span>{comment.text}</span>
+          </div>
+          <div className={styles.infRebox}>
+            <div className={styles.idbox}>
+              <span className={styles.date}>20230646</span>{" "}
+              <span className={styles.id}>영화과 ㅇ채욱</span>
             </div>
-         ))}
-      </>
-   );
+            <div className={styles.reBtn}>
+              {comment.text &&
+                comment.text.split(' ').map((word, voteIdx) => (
+                  <div key={voteIdx}>
+                    <BsFillHandThumbsUpFill
+                      className='rePBtn'
+                      onClick={() => upHandler(commentIdx, voteIdx)}
+                    />
+                    <span> {voteData[commentIdx][voteIdx].upVotes}</span>
+                    <BsFillHandThumbsDownFill
+                      className='rePBtn'
+                      onClick={() => downHandler(commentIdx, voteIdx)}
+                    />
+                    <span> {voteData[commentIdx][voteIdx].downVotes}</span>
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default AboutMap;
+
